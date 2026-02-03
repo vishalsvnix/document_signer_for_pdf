055256996ac9f6cb645087079f5cd8c997935d85
vishal sharma <vishal.sharma@meddo.in>
2026-02-03 23:45:21 +0530

    bug fixed

--- document_signer_for_pdf/__init__.py
+++ document_signer_for_pdf/__init__.py
@@ -1,8 +1,7 @@
-
 __version__ = '0.0.1'
 
-import importlib
-
-pyhanko_spec = importlib.util.find_spec("pyhanko")
-if pyhanko_spec is not None:
+try:
     from document_signer_for_pdf.document_signer_for_pdfs.patches import pdf
+except ImportError:
+    # Dependencies not yet installed (during bench build)
+    pass
